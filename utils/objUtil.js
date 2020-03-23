a1c4a1fb699ca12a59749c5c013cee9213eeb3d2
anh trần <trananhpm94@gmail.com>
2020-03-23 16:35:18 +0700

    anhtt fix pick util

--- utils/objUtil.js
+++ utils/objUtil.js
@@ -4,7 +4,13 @@ export const pick = (sourceObject, keys) => {
   }
   const newObject = {};
   (keys || []).forEach(key => {
-    newObject[key] = sourceObject[key];
+    if (typeof key === 'string') {
+      newObject[key] = sourceObject[key];
+    } else if (typeof key === 'object') {
+      Object.entries(key).forEach(([oldKey, newKey]) => {
+        newObject[newKey] = sourceObject[oldKey];
+      });
+    }
   });
   return newObject;
 };
