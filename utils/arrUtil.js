f3a1c928edc52d4419f2da61bc66cdf72d2dff8e
anh trần <trananhpm94@gmail.com>
2020-05-12 11:53:49 +0700

    anhtt add arrUtil

--- utils/arrUtil.js
+++ utils/arrUtil.js
@@ -1,10 +1,10 @@
-const groupBy = (source, key) => {
+export const groupBy = (source, key) => {
   return source.reduce((rv, item) => {
     (rv[item[key]] = rv[item[key]] || []).push(item);
     return rv;
   }, {});
 };
 
-const distinctByKey = (array, key) => {
+export const distinctByKey = (array, key) => {
   return [...new Map(array.map((item) => [item[key], item])).values()];
 };
