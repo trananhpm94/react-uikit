27b47cce304840353b2b6c503e5085e842fcf981
tungthai <thajcaca@gmail.com>
2020-05-19 09:46:11 +0700

    fix bug reset label component select multible

--- component/input/SelectMultiAjax.js
+++ component/input/SelectMultiAjax.js
@@ -31,12 +31,11 @@ export default class SelectMultiAjax extends Component {
     if (!objectEquals(nextProps.params, this.props.params)) {
       this.actionGetData(nextProps);
     }
-    if (nextProps.resetDataLabel !== this.props.resetDataLabel) {
+    if (!nextProps.value) {
       this.setState({
         placeholder: false,
       });
     }
-
     this.checkValueNumber(nextProps);
     this.checkShowCount(nextProps);
   };
