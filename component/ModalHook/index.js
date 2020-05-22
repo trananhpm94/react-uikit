01eeab20ca597e732b0db8fca900276b30a82757
Anh Tran <trananhpm94@gmail.com>
2020-05-22 16:10:04 +0700

    anhtt fix modal hook

--- component/ModalHook/index.js
+++ component/ModalHook/index.js
@@ -4,13 +4,25 @@ import useModal from './useModal';
 
 export default ({ id, title = '', ...props }) => {
   const [modalData = {}, , hiddenModal] = useModal(id);
+  const onOk = async () => {
+    if (props.onOk) {
+      const result = await props.onOk();
+      if (result !== false) {
+        hiddenModal();
+      }
+    } else {
+      hiddenModal();
+    }
+  };
   return (
     <Modal
       title={title}
+      cancelText="Hủy"
       visible={modalData.visible}
       onCancel={() => hiddenModal()}
       style={{ top: 20 }}
       {...props}
+      onOk={onOk}
     >
       {props.children}
     </Modal>
